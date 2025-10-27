4d453bcb60df561536bdbe51fdb30e19491b981a
Yoonhojoon <dbsghwns1209@gmail.com>
2025-10-27 17:00:57 +0900

    feat(Car): 자동차 클래스 추가
    - 자동차의 이름과 위치를 관리하는 Car 클래스 구현
    - 이동 규칙에 따라 자동차의 위치를 업데이트하는 move 메서드 추가
    - 자동차 이름과 현재 위치를 반환하는 getter 메서드 포함 (리팩토링 예정)

---
+++ src/main/java/racingcar/Car.java
@@ -0,0 +1,25 @@
+package racingcar;
+
+public class Car {
+    private final String name;
+    private int position;
+
+    public Car(String name) {
+        this.name = name;
+        this.position = 0;
+    }
+
+    public void move(MoveRule moveRule, int randomValue) {
+        if (moveRule.canMove(randomValue)) {
+            position++;
+        }
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getPosition() {
+        return position;
+    }
+}
